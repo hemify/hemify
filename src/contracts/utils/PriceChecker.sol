523d034f6fdcc8277e3d4e8baabc3687f575d62c
fps <anthony.nnaemeka.umeh@gmail.com>
2023-05-02 07:50:56 +0100

    Moved OpenAuctionV1.sol to OpenAuctionV1 folder for organization.

--- src/contracts/utils/PriceChecker.sol
+++ src/contracts/utils/PriceChecker.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.19;
 
 import {AggregatorV3Interface}
-from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+    from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
